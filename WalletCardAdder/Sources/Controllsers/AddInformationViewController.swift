5583c2ec04075a14033153ca01ddea5c53d3e5a2
byongguen son <ios@sch.ac.kr>
2018-08-26 02:31:53 +0900

    파일 경로 수정

---
+++ WalletCardAdder/Sources/Controllsers/AddInformationViewController.swift
@@ -0,0 +1,116 @@
+//
+//  AddInformationViewController.swift
+//  WalletCardAdder
+//
+//  Created by byongguen son on 2018. 8. 25..
+//  Copyright © 2018년 TrueSage. All rights reserved.
+//
+
+import UIKit
+import Vision
+class AddInformationViewController : UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    @IBOutlet weak var barcodeImageView: UIImageView!
+    @IBOutlet weak var cardView: UIView!
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var subContentLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    let picker = UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        
+        self.contentLabel.isUserInteractionEnabled = true
+        self.subContentLabel.isUserInteractionEnabled = true
+        self.dateLabel.isUserInteractionEnabled = true
+        self.contentLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(changeContent)))
+        self.subContentLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(changSubContent)))
+        self.dateLabel.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(changeDate)))
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        UIView.animate(withDuration: 0.5) {
+            self.view.alpha = 1
+            self.view.layoutIfNeeded()
+        }
+    }
+    @IBAction func addBarcodeImageAction(_ sender: UIButton) {
+        self.present(self.picker, animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerControllerEditedImage] as? UIImage else {
+            print("No image found")
+            return
+        }
+        
+        self.getBarcodeFromImage(image)
+    }
+    func getBarcodeFromImage(_ image: UIImage){
+        
+        let detector:CIDetector=CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [CIDetectorAccuracy:CIDetectorAccuracyHigh])!
+        var decode=""
+        let ciImage:CIImage = CIImage(image:image)!
+        var qrCodeLink=""
+        
+        let features=detector.features(in: ciImage)
+        for feature in features as! [CIQRCodeFeature] {
+            qrCodeLink += feature.messageString!
+        }
+        print(qrCodeLink)
+        let data = qrCodeLink.data(using: String.Encoding.ascii)
+        //CIPDF417BarcodeGenerator
+        //CICode128BarcodeGenerator
+        //CICodeQRCodeGenerator
+        if let filter = CIFilter(name: "CIPDF417BarcodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 3, y: 3)
+            
+            if let output = filter.outputImage?.transformed(by: transform) {
+                
+                self.barcodeImageView.image =  UIImage(ciImage: output)
+            }
+        }
+    }
+    @IBAction func addLogoImage(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func addCardAction(_ sender: UIButton) {
+    }
+    @objc func changeDate(){
+        self.getChangedText(dateLabel)
+    }
+    @objc func changeContent(){
+         self.getChangedText(contentLabel)
+    }
+    @objc func changSubContent(){
+         self.getChangedText(subContentLabel)
+    }
+    func getChangedText(_ label: UILabel){
+        let alert = UIAlertController(title: "수정", message: "수정할 문자열을 입력하세요", preferredStyle: .alert)
+        alert.addTextField { (tf) in
+            tf.placeholder = "문자열 입력"
+        }
+        let okAction = UIAlertAction(title: "확인", style: .default) { (_) in
+            label.text = (alert.textFields?.first?.text!)!
+        }
+        alert.addAction(okAction)
+        self.present(alert,animated: true)
+    }
+    @IBAction func closeAction(){
+        self.dismiss(animated: false, completion: nil)
+    }
+}
+@available(iOS 11.0, *)
+extension AddInformationViewController{
+   
+}
