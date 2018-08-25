c38fc0f4b7db4ce361900995fe1f1cc1c1c542d2
truesage <truesage85@gmail.com>
2018-08-25 15:32:32 +0900

    [월렛카드추가기] 프로젝트 생성.

---
+++ WalletCardAdder/WalletCardAdder/Sources/Base/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  Jin-Memo
+//
+//  Created by Jin-MackBookPro15 on 2018. 8. 5..
+//  Copyright © 2018년 TrueSage. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController, BaseViewProtocol {
+
+    // MARK: - Public Function
+    
+    // MARK: - Private Function
+    
+
+}
+
---
+++ WalletCardAdder/WalletCardAdder/Sources/Base/BaseViewProtocol.swift
@@ -0,0 +1,10 @@
+//
+// Created by Jin-MackBookPro15 on 2018. 8. 5..
+// Copyright (c) 2018 TrueSage. All rights reserved.
+//
+
+import Foundation
+
+protocol BaseViewProtocol {
+    //preconditionFailure("This method must be overridden")
+}
